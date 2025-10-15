2330e14509e4f28c5c188ff97d8ea47b60fe9547
Emiliano Debenedetti <emilianodebenedetti@outlook.com>
2025-10-15 02:01:53 -0300

    Termino de configurar dependencias en el frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// src/main.jsx (o App.jsx)
+import 'flowbite';
+import './index.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
